a74b884262edce4c83e2de8a7b4070b9e719177f
RIANAEH <ririhan217@gmail.com>
2021-11-20 21:58:14 +0900

    style: 필요없는 url 삭제

--- matching/urls.py
+++ matching/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path('infos', MatchingInfoView.as_view()),
     path('infos/<int:uid>', MatchingInfoView.as_view()),
     path('results', MatchingResultView.as_view()),
-    path('results/<int:uid>', MatchingResultView.as_view()),
-    path('matching_result/', views.matching_result),
+    path('results/<int:uid>', MatchingResultView.as_view())
 ]
