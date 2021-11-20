588c66d859a5bdd93060fcf1b81582779258f813
RIANAEH <ririhan217@gmail.com>
2021-11-20 19:30:50 +0900

    feat: user 테이블 추가, 일부 데이터 스키마 변경

--- matching/models.py
+++ matching/models.py
@@ -1,26 +1,33 @@
 from django.db import models
 
-class MatchingInfo(models.Model):
+class User(models.Model):
     id = models.IntegerField(primary_key=True)
+    use_matching = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'user'
+
+class MatchingInfo(models.Model):
+    id = models.AutoField(primary_key=True)
+    uid = models.ForeignKey(User, related_name="matching_info", on_delete=models.CASCADE, db_column="uid")
     sex = models.IntegerField()
     age = models.FloatField()
     mbti = models.CharField(max_length=10)
     user_smoking = models.IntegerField()
     mate_smoking = models.FloatField()
     user_pet = models.IntegerField()
-    user_pet_dog = models.IntegerField()
-    user_pet_cat = models.IntegerField()
-    user_pet_reptile_fish = models.IntegerField()
-    user_pet_rodent = models.IntegerField()
-    user_pet_bird = models.IntegerField()
+    user_pet_dog = models.IntegerField(default=0)
+    user_pet_cat = models.IntegerField(default=0)
+    user_pet_reptile_fish = models.IntegerField(default=0)
+    user_pet_rodent = models.IntegerField(default=0)
+    user_pet_bird = models.IntegerField(default=0)
     mate_pet = models.FloatField()
-    mate_pet_dog = models.FloatField()
-    mate_pet_cat = models.FloatField()
-    mate_pet_reptile_fish = models.FloatField()
-    mate_pet_rodent = models.FloatField()
-    mate_pet_bird = models.FloatField()
-    air_like_airconditioner = models.IntegerField()
-    air_night_airconditioner = models.IntegerField()
+    mate_pet_dog = models.FloatField(default=0)
+    mate_pet_cat = models.FloatField(default=0)
+    mate_pet_reptile_fish = models.FloatField(default=0)
+    mate_pet_rodent = models.FloatField(default=0)
+    mate_pet_bird = models.FloatField(default=0)
+    air_like_airconditioner = models.FloatField()
     air_like_heater = models.FloatField()
     noise_talking = models.FloatField()
     noise_music = models.FloatField()
@@ -36,14 +43,13 @@ class MatchingInfo(models.Model):
     permission_to_enter = models.FloatField()
 
     class Meta:
-        managed = False
         db_table = 'matching_info'
 
 class MatchingResult(models.Model):
-    uid = models.IntegerField()
+    id = models.AutoField(primary_key=True)
+    uid = models.ForeignKey(User, related_name="matching_result", on_delete=models.CASCADE, db_column="uid")
     mate_list = models.CharField(max_length=1000)
     update_at = models.DateTimeField()
 
     class Meta:
-        managed = False
         db_table = 'matching_result'
