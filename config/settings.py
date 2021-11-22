ca708f49a5dc2f264f64612a243230c78cf03dc5
RIANAEH <ririhan217@gmail.com>
2021-11-23 02:14:13 +0900

    fix: change EC2 host address

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '8uc-@h30se25spz-tj)3_2=1%$z)2fl%nm_!30*^u!ajea!2ap'
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    ".ap-northeast-2.compute.amazonaws.com"
+    ".us-east-2.compute.amazonaws.com"
 ]
 
 
